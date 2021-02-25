cf24a0ecb6a71452dd02f7b9b8f887f5172ecb10
3807558 <lawrence.lockhart@fedex.com>
2021-02-25 06:00:12 -0600

    Linked List implementation w/own class

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+	    Node node0 = new Node(223);
+	    System.out.println("Node0 has a value of "+node0+ "and next node value set to "+node0.getNext());
+
+    }
+}
---
+++ src/com/company/Node.java
@@ -0,0 +1,34 @@
+package com.company;
+
+public class Node {
+    private Node next;
+    private int value;
+
+    public Node(int val) {
+        value = val;
+        next = null;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "value=" + value +
+                '}';
+    }
+}
