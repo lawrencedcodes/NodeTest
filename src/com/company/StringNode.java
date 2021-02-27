b292558f2d0a98e9972e25ec70225ceca4eba806
3807558 <lawrence.lockhart@fedex.com>
2021-02-26 23:26:46 -0600

    add stringnode class

---
+++ src/com/company/StringNode.java
@@ -0,0 +1,32 @@
+package com.company;
+
+public class StringNode {
+    public StringNode next;
+    public String value;
+
+    public StringNode(String word) {
+        value = word;
+        next = null;
+    }
+
+    public StringNode getNext() {
+        return next;
+    }
+
+    public void setNext(StringNode next) {
+        this.next = next;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Node string is "+ value;
+    }
+}
