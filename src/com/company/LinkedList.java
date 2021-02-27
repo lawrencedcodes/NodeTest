d369e07488c1c6a26e8612d41b99aee1c0127860
Lawrence Lockhart <ldlockhart@gmail.com>
2021-02-27 07:47:57 -0600

    basic linkedlist class

--- src/com/company/LinkedList.java
+++ src/com/company/LinkedList.java
@@ -1,5 +1,15 @@
 package com.company;
 
 public class LinkedList {
+    private Node hdptr;
 
+    public LinkedList() {
+        hdptr = null;
+    }
+
+    public void insFirst(int data) {
+        Node node0 = new Node(data);
+        node0.setNext(hdptr);
+        hdptr = node0;
+    }
 }
